31e983a332ad30692206bd42b5ff230fa39d288f
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-06-26 20:18:49 +0530

    Time: 3 ms (81.15%), Space: 6.8 MB (54.05%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<int> stk;
-        
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(') stk.push(i);
-            else{
-                if(stk.size()==1){
-                    s[stk.top()]='0';
-                    s[i]='0';
-                }
-                stk.pop();
-            }
-        }
-        
+        int count=0;
         string ans;
         for(auto &it:s)
-            if(it!='0')
-                ans.push_back(it);
-        
+        {
+            if(it=='('){
+                if(count) ans.push_back(it);
+                count++;
+            }else{
+                count--;
+                if(count) ans.push_back(it);
+            }
+        }
         return ans;
     }
 };
