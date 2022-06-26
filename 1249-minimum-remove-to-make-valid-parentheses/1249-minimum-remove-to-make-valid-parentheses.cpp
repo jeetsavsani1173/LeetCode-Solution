8ef42a24e0b5e156d3217625b06a7eab99d2ffe0
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-06-26 18:04:35 +0530

    Time: 22 ms (86.86%), Space: 12.1 MB (28.67%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stk;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+                stk.push(i);
+            else if(s[i]==')')
+            {
+                if(stk.size()==0) stk.push(i);
+                else if(s[stk.top()]=='(') stk.pop();
+                else stk.push(i);
+            }
+        }
+        while(!stk.empty())
+        {
+            s[stk.top()]='0';
+            stk.pop();
+        }
+        
+        string ans;
+        for(auto it:s)
+            if(it!='0')
+                ans.push_back(it);
+        
+        return ans;
+    }
+};
