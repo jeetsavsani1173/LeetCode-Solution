7ed715f737e886ebfcd1cd323e32854cce4aec75
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-06-27 21:50:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (76.99%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<char> stk;
+        
+        for(auto &it:s)
+        {
+            if(it!=')') stk.push(it);
+            else{
+                string temp;
+                while(stk.top()!='(')
+                {
+                    temp.push_back(stk.top());
+                    stk.pop();
+                }
+                
+                stk.pop(); // remove opening brace..
+                for(auto &it:temp)
+                    stk.push(it);
+            }
+        }
+        
+        string ans;
+        while(!stk.empty()){
+            ans+=stk.top();
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
