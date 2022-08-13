efe64caa20591d7e1119909b0cfa25a37aac7b76
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-08-13 08:27:08 +0530

    Time: 182 ms (77.10%), Space: 16.9 MB (86.00%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -9,35 +9,13 @@ public:
         vector<int> hash(26,0);
         for(auto it:words) for(auto it1:it) hash[it1-'a']++;
         int Window_Size=words.size()*words[0].size();
-        // cout<<Window_Size<<endl;
         vector<int> W_hash(26,0);
         map<string,int> mps;
         for(auto it:words) mps[it]++;
-        // for(auto it:mps) cout<<it.first<<" -> "<<it.second<<endl;
         if(s.size()<Window_Size) return {};
         for(int i=0;i<Window_Size;i++) W_hash[s[i]-'a']++;
         vector<int> ans;
         
-        // if(issame(W_hash,hash)){
-        //         map<string,int> mp=mps;
-        //         int pos=0;
-        //         int end=0+Window_Size;
-        //         bool flag=true;
-        //         while(pos<end){
-        //             string temp=s.substr(pos,words[0].size());
-        //             // cout<<temp<<endl;
-        //             pos+=words[0].size();
-        //             // for(auto it:mp) cout<<it.first<<" -> "<<it.second<<endl;
-        //             if(mp[temp]==0){
-        //                 flag=false;
-        //                 break;
-        //             }else{
-        //                 mp[temp]--;
-        //             }
-        //         }
-        //         for(auto it:mp) cout<<it.first<<" -> "<<it.second<<endl;
-        //         if(flag) ans.push_back(0);
-        //     }
         
         int i=0;
         for(;i<=s.size()-Window_Size;i++)
@@ -49,9 +27,7 @@ public:
                 bool flag=true;
                 while(pos<end){
                     string temp=s.substr(pos,words[0].size());
-                    // cout<<temp<<endl;
                     pos+=words[0].size();
-                    // for(auto it:mp) cout<<it.first<<" -> "<<it.second<<endl;
                     if(mp[temp]==0){
                         flag=false;
                         break;
