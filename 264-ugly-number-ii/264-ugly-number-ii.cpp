77e266801b14546b0c206fe70230bee5c0951d93
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-08-16 12:57:43 +0530

    Time: 85 ms (29.21%), Space: 10.7 MB (23.92%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> dp;
-        dp.push_back(0);
-        dp.push_back(1);
-        int ptr2=1,ptr3=1,ptr5=1;
-        
-        for(int i=2;i<=n;i++)
+        priority_queue<long, vector<long>, greater<long> > pq;
+        pq.push(1);
+        int nth=0;
+        long val;
+        while(!pq.empty() && nth<n)
         {
-            int a=dp[ptr2]*2;
-            int b=dp[ptr3]*3;
-            int c=dp[ptr5]*5;
-            
-            int mn=min(a,min(b,c));
-            dp.push_back(mn);
-            
-            if(mn==a) ptr2++;
-            if(mn==b) ptr3++;
-            if(mn==c) ptr5++;
+            val=pq.top();
+            pq.pop();
+            while(!pq.empty() && val==pq.top()) 
+                pq.pop();
+            nth++;
+            pq.push(2ll*val);
+            pq.push(3ll*val);
+            pq.push(5ll*val);
         }
-        
-        return dp[n];
+        return val;
     }
 };
