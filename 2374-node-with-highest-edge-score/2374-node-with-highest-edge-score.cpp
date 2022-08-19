9c9db1f190499d771a1c552fd308f2100a226ab6
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2022-08-19 07:59:14 +0530

    Time: 236 ms (81.57%), Space: 110 MB (50.02%) - LeetHub

---
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int edgeScore(vector<int>& edges) {
+        vector<long long> scores(edges.size(),0);
+        
+        for(int i=0;i<edges.size();i++)
+            scores[edges[i]]+=i;
+        
+        long long mx_score=*max_element(scores.begin(),scores.end());
+        
+        for(int i=0;i<scores.size();i++)
+            if(scores[i]==mx_score) 
+                return i;
+        return 0;
+    }
+};
