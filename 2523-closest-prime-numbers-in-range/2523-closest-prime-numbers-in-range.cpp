d158eea1bcc5d33c1391f0a1e3bf5072c53457a1
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-01-04 20:00:29 +0530

    Time: 218 ms (78.14%), Space: 20.1 MB (38.76%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<bool> seives(int n)
+    {
+        vector<bool> is_prime(n + 1, true);
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (is_prime[i]) {
+                for (int j = i * i; j <= n; j += i)
+                    is_prime[j] = false;
+            }
+        }
+        return is_prime;
+    }
+    vector<int> closestPrimes(int left, int right) {
+        if(left==right) return {-1,-1};
+        vector<bool> seive=seives(right+1);
+        vector<int> primes;
+        for(int i=left;i<=right;i++)
+            if(seive[i]){
+                primes.push_back(i);
+            }
+        if(primes.size()==0 || primes.size()==1) return {-1,-1};
+        
+        int mn=INT_MAX;
+        
+        for(int i=0;i<primes.size()-1;i++)
+        {
+            mn=min(mn,primes[i+1]-primes[i]);
+        }
+        
+        if(mn==INT_MAX) return {-1,-1};
+        
+        for(int i=0;i<primes.size()-1;i++)
+        {
+            if(primes[i+1]-primes[i]==mn){
+                return {primes[i],primes[i+1]};
+            }
+        }
+        return {};
+    }
+};
