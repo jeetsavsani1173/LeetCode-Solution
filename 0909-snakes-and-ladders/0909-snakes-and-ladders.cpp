64f39cbbb76c0d8fbc748677ecbee7433e709a65
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-03-02 16:50:42 +0530

    Time: 65 ms (12.18%), Space: 23.9 MB (5.05%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,39 +1,65 @@
 class Solution {
 public:
-    pair<int,int> find_cordinate(int temp,int n)
-    {
-        int row=n-(temp-1)/n-1;
-        int col=((temp-1)%n);
-        if(n%2==row%2){
-            col=n-1-col;
-        }
-        return {row,col};
-    }
     int snakesAndLadders(vector<vector<int>>& board) {
         int n=board.size();
-        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        int level=0,cell=1;
+        vector<int> vect;
+        vect.push_back(0);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(level%2==0){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]==-1) vect.push_back(cell);
+                    else vect.push_back(board[i][j]);
+                    cell++;
+                }
+            }else{
+                for(int j=n-1;j>=0;j--){
+                    if(board[i][j]==-1) vect.push_back(cell);
+                    else vect.push_back(board[i][j]);
+                    cell++;
+                }
+            }
+            level++;
+        }
+        
+        // Generate a Graph from grid
+        vector<int> graph[n*n+1];
+        for(cell=1;cell<=n*n;cell++)
+        {
+            for(int move=1;move<=6 && cell+move<=(n*n);move++){
+                if(cell==vect[cell+move]) continue;
+                graph[cell].push_back(vect[cell+move]);
+            }
+        }
+        // for(int i=1;i<vect.size();i++)
+        // {
+        //     cout<<i<<"-> ";
+        //     for(auto it:graph[i]){
+        //         cout<<it<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // Apply BFS 
+        vector<int> vis(n*n+1,-1);
         queue<pair<int,int>> q;
         q.push({1,0});
         
-        while(!q.empty())
-        {
-            pair<int,int> x=q.front();
+        while(!q.empty()){
+            int node=q.front().first;
+            int dis=q.front().second;
             q.pop();
-            if(x.first==n*n) return x.second;
-            
-            for(int k=1;k<=6;k++)
-            {
-                if((k+x.first)>n*n) break;
-                pair<int,int> newP=find_cordinate(k+x.first,n);
-                if(vis[newP.first][newP.second]==true) continue;
-                vis[newP.first][newP.second]=true;
-                if(board[newP.first][newP.second]!=-1) 
-                    q.push({board[newP.first][newP.second],x.second+1}); 
-                else
-                    q.push({x.first+k,x.second+1}); 
+            if(vis[node]!=-1) continue;
+            vis[node]=dis;
+            for(auto it:graph[node]){
+                if(vis[it]==-1){
+                    q.push({it,dis+1});
+                }
             }
         }
-        
-        return -1;
+        // for(auto it:vis)
+        //     cout<<it<<" ";
+        return vis[n*n];
     }
 };
