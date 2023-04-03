5fb83ce2d40bbd0226c02f0a5bb18dbc0f8c01cd
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-04 00:28:11 +0530

    Time: 80 ms (92.62%), Space: 23.2 MB (6.47%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,16 +4,12 @@ public:
         int n=matrix.size(),m=matrix[0].size();
         vector<vector<int>> dp(n,vector<int>(m,0));
         
-        // for(int j=0;j<m;j++) dp[0][j]=matrix[0][j]-'0';
-        // for(int i=0;i<n;i++) dp[i][0]=matrix[i][0]-'0';
+        for(int j=0;j<m;j++) dp[0][j]=matrix[0][j]-'0';
+        for(int i=0;i<n;i++) dp[i][0]=matrix[i][0]-'0';
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0) {
-                    dp[0][j]=matrix[0][j]-'0';
-                }else if(j==0){
-                    dp[i][0]=matrix[i][0]-'0';
-                }else if(matrix[i][j]=='1'){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]=='1'){
                     dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
                 }else{
                     dp[i][j]=0;
