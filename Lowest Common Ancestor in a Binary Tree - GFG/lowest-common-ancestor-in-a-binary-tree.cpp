b97e6689c0a1f67b2a0093c5b54e62305176e4d9
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-03 20:59:36 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -40,8 +40,7 @@ class Solution
     Node* dfs(Node* root,int n1,int n2)
     {
         if(root==NULL) return NULL;
-        if(root->data==n1) return root;
-        if(root->data==n2) return root;
+        if(root->data==n1 || root->data==n2) return root;
         
         Node* l=dfs(root->left,n1,n2);
         Node* r=dfs(root->right,n1,n2);
