254188226a73302f9f7f7df393e1781ff917afb6
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-08 19:22:53 +0530

    Time: 2216 ms (5.01%), Space: 347.3 MB (5.01%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    class Compare {
+    public:
+        bool operator()(vector<int> &a, vector<int> &b)
+        {
+            return a[2]>=b[2];
+        }
+    };
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         int n=0;
         for(auto it:routes) for(auto it1:it) n=max(n,it1);
@@ -14,17 +21,17 @@ public:
             }
         }
         
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        pq.push({0,source,(int)routes.size()});
+        priority_queue<vector<int>,vector<vector<int>>,Compare> pq;
+        pq.push({source,(int)routes.size(),0});
         
         vector<vector<int>> vis(n+1,vector<int>(routes.size()+1,-1));
         
         while(!pq.empty()){
             auto tp=pq.top();
             pq.pop();
-            int stop=tp[1];
-            int color=tp[2];
-            int changes=tp[0];
+            int stop=tp[0];
+            int color=tp[1];
+            int changes=tp[2];
             
             if(stop==target) return changes;
             
@@ -33,9 +40,9 @@ public:
             
             for(auto nbr:graph[stop]){
                 if(color==nbr[1]){
-                    pq.push({changes,nbr[0],nbr[1]});
+                    pq.push({nbr[0],nbr[1],changes});
                 }else{
-                    pq.push({changes+1,nbr[0],nbr[1]});
+                    pq.push({nbr[0],nbr[1],changes+1});
                 }
             }
         }
