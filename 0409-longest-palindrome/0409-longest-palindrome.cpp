d8fdae6aa9535254dc9230439c054c9cf878128e
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-08 08:06:34 +0530

    Time: 7 ms (21.32%), Space: 6.8 MB (19.55%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int odd=0;
+        int ans=0;
+        map<int,int> mp;
+        for(auto it:s) mp[it]++;
+        
+        for(auto it:mp)
+        {
+            if(it.second%2==1) odd++;
+            ans+=((it.second/2)*2);
+        }
+        if(odd) ans++;
+        return ans;
+    }
+};
