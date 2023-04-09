172c23d07f1439179f00ed0fe9288b06b11e3673
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-09 17:25:01 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -15,7 +15,7 @@ class Solution {
 	    for(int k=0;k<n;k++){
 	        for(int i=0;i<n;i++){
 	            for(int j=0;j<n;j++){
-	                if(i==j) continue;
+	               // if(i==j) continue;
 	                if(matrix[i][k]!=-1 && matrix[k][j]!=-1){
 	                    if(matrix[i][j]==-1){
 	                        matrix[i][j]=matrix[i][k]+matrix[k][j];
