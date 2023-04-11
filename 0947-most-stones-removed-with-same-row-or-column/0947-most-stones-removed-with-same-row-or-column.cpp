da1d8b567c8982b12664c4c869eb8859fa015845
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-11 19:38:20 +0530

    Time: 58 ms (59.03%), Space: 21.8 MB (16.61%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,53 +1,80 @@
-class Solution {
+class DisjointSet{
 public:
-    vector<int> graph[1005];
-    
-    void dfs(int node,int &size,vector<bool> &vis)
-    {
-        vis[node]=true;
-        size++;
-        for(auto it:graph[node])
-        {
-            if(!vis[it])
-                dfs(it,size,vis);
+vector<int> rank,parent,size;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++)
+            parent[i]=i;
+    }
+
+    // find Ultimate Parent with path compression..
+    int findUPar(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // Union by Rank..
+    void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }else if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
         }
     }
+
+    // Union by Size
+    void unionBySize(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
-                {
-                    graph[i+1].push_back(j+1);
-                    graph[j+1].push_back(i+1);
-                }
-            }
+        int maxRow=0;
+        int maxCol=0;
+        
+        for(auto it:stones){
+            maxRow=max(maxRow,it[0]);
+            maxCol=max(maxCol,it[1]);
         }
         
-        // for(int i=1;i<=n;i++)
-        // {
-        //     cout<<i<<" -> ";
-        //     for(auto it:graph[i])
-        //         cout<<it<<" ";
-        //     cout<<endl;
-        // }
+        DisjointSet ds(maxRow+maxCol+2);
+        map<int,int> mp;
+        for(auto it:stones){
+            int nodeRow=it[0];
+            int nodeCol=maxRow+it[1]+1;
+            ds.unionBySize(nodeRow,nodeCol);
+            mp[nodeRow]=1;
+            mp[nodeCol]=1;
+        }
         
         int ans=0;
-        vector<bool> vis(n+1,false);
-        
-        for(int i=1;i<=n;i++)
-        {
-            if(!vis[i])
-            {
-                int size=0;
-                dfs(i,size,vis);
-                
-                ans+=(size-1);
+        for(auto it:mp){
+            if(ds.findUPar(it.first)==it.first){
+                ans++;
             }
         }
-        
-        return ans;
+        return n-ans;
     }
 };
