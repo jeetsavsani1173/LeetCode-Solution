74b0629b2d84ae7b774161893032fa04e3bd17b5
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-17 08:06:58 +0530

    Time: 6 ms (29.92%), Space: 5.8 MB (93.27%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans;
+        
+        for(int i=0;i<32;i++)
+        {
+            ans<<=1;
+            ans+=n%2;
+            n>>=1;
+        }
+        return ans;
+    }
+};
