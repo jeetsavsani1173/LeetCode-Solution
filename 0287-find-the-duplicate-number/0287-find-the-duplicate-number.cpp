4a350cf4d692ac38bcf49da18f2102d8a2184d0a
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-04-29 12:10:17 +0530

    Time: 135 ms (47.16%), Space: 61.2 MB (93.56%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // use slow and fast pointer approach to find the cycle in LL.
+    // if there is any duplicate then it will always create cycle that for sure..
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        // find the cycle 
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        
+        // finding the intersect point of cycle..
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
