19a9de753fff30106282c8273610ff22d9815d91
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-04 16:38:51 +0530

    Time: 163 ms (71.91%), Space: 23.9 MB (92.96%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,30 +6,25 @@ public:
         vector<vector<int>> ans;
         if(n<=2) return ans;
         
-        for(int i=0;i<n;i++)
-        {
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             
             int target=-1*nums[i];
-            //Two-pointer for unique pair..
+            
             int left=i+1,right=n-1;
-            while(left<right)
-            {
-                if(nums[left]+nums[right]==target)
-                {
+            while(left<right){
+                if((nums[left]+nums[right])==target){
                     ans.push_back({nums[i],nums[left],nums[right]});
-                    int temp=nums[left];// remove of duplicate
-                    while(left<n && nums[left]==temp)
-                        left++;
-                }
-                else if(nums[left]+nums[right]>target)
+                    int temp=nums[left];
+                    while(left<n && nums[left]==temp) left++;
                     right--;
-                else
+                }else if((nums[left]+nums[right])<target){
                     left++;
+                }else{
+                    right--;
+                }
             }
         }
-        
         return ans;
     }
 };
