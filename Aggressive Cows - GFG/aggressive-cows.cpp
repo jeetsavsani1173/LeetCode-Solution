9c7953233980f9fdff60e3d068e5e058e9468ae1
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-09 10:18:56 +0530

    Added solution - LeetHub

---
+++ Aggressive Cows - GFG/aggressive-cows.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int check(vector<int> &stalls,int gap)
+    {
+        int cows=1;
+        int prev=0;
+        
+        for(int i=1;i<stalls.size();i++){
+            if((stalls[i]-stalls[prev])>=gap){
+                prev=i;
+                cows++;
+            }
+        }
+        return cows;
+    }
+    int solve(int n, int k, vector<int> &stalls) {
+        sort(stalls.begin(),stalls.end());
+        int l=0,r=1e9+5;
+        
+        int ans=-1;
+        
+        while(l<=r){
+            int mid=(r-l)/2+l;
+            if(check(stalls,mid)>=k){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
