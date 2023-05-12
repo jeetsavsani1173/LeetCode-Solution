a1792de8bbe71ef30478111c0f522d0444629eab
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-12 07:45:56 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void addAtRightPosition(stack<int> &st,int val,int prev)
+{
+    if(st.size()==0){
+        if(val<=prev)
+            st.push(val);
+        return;
+    }
+    int currVal=st.top();
+    st.pop();
+    addAtRightPosition(st,val,currVal);
+    st.push(currVal);
+    if(val<=prev && val>currVal)
+        st.push(val);
+}
+void sortStack(stack<int> &stack){
+    if(stack.size()==0) return;
+    int val=stack.top();
+    stack.pop();
+    sortStack(stack);
+    addAtRightPosition(stack,val,1e9+5);
+}
+void SortedStack :: sort()
+{
+    sortStack(s);
+}
