ef5cbbe7d01200c161aa527e0830f6bd74c5a60b
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-12 10:31:20 +0530

    Time: 25 ms (12.89%), Space: 11.3 MB (26.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -10,8 +10,6 @@ public:
             }
             return;
         }
-        // if(target<0)
-        //     return;
         // take then check howmany times 0 or 1 or 2 or 3 ....
         int freq=mp[num[idx]];
         for(int i=0;i<=freq;i++)
@@ -27,7 +25,6 @@ public:
             for(int j=0;j<i;j++) 
                 temp_ans.pop_back(); // back-Track
         }
-        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int> temp_ans;
