9f4203ae2ead118ed4fe97aabfc8a60d5d7408ef
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-16 08:19:18 +0530

    Time: 27 ms (5.23%), Space: 18.6 MB (5.43%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stk;
+        stk.push(asteroids[0]);
+        for(int i=1;i<asteroids.size();i++){
+            if(asteroids[i]>=0){ // positive
+                if(!stk.empty() && stk.top()<0){ // negative
+                    // collision
+                    stk.push(asteroids[i]);
+                    while(stk.size()>=2){
+                        int tp1=stk.top();
+                        stk.pop();
+                        int tp2=stk.top();
+                        stk.pop();
+                        if((tp2<0 && tp1>0) || (tp1*tp2)>0){
+                            stk.push(tp2);
+                            stk.push(tp1);
+                            break;
+                        }else if(abs(tp1)>abs(tp2)){
+                            stk.push(tp1);
+                        }else if(abs(tp1)<abs(tp2)){
+                            stk.push(tp2);
+                        }
+                    }
+                }else{ // positive
+                    // push
+                    stk.push(asteroids[i]);
+                }
+                // stk.push(asteroids[i]);
+            }else{ // negative
+                if(!stk.empty() && stk.top()>=0){ // positive
+                    // collision
+                    stk.push(asteroids[i]);
+                    while(stk.size()>=2){
+                        int tp1=stk.top();
+                        stk.pop();
+                        int tp2=stk.top();
+                        stk.pop();
+                        if((tp2<0 && tp1>0) || (tp1*tp2)>0){
+                            stk.push(tp2);
+                            stk.push(tp1);
+                            break;
+                        }else if(abs(tp1)>abs(tp2)){
+                            stk.push(tp1);
+                        }else if(abs(tp1)<abs(tp2)){
+                            stk.push(tp2);
+                        }
+                    }
+                }else{ // negative
+                    // push
+                    stk.push(asteroids[i]);
+                }
+            }
+        }
+        vector<int> ans;
+        while(!stk.empty()){
+            ans.push_back(stk.top());
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
