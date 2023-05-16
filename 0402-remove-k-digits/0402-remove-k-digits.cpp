44a748c8026bd2400299a1df12c1c25828a59e76
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-16 09:36:36 +0530

    Time: 18 ms (32.61%), Space: 8.8 MB (51.76%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        
+        for(int i=0;i<num.size();i++)
+        {
+            char curr=num[i];
+            
+            while(!st.empty() && k>0 && curr<st.top())
+            {
+                st.pop();
+                k--;
+            }
+            // for ignoring leading zero's
+            if(!st.empty() || curr!='0')
+                st.push(curr);
+        }
+        while(!st.empty() && k--)
+            st.pop();
+        if(st.size()==0)
+            return "0";
+        
+        string ans;
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
