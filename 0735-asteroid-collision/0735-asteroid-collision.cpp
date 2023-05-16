b4b837f153ec350b1dbb07bb307e4a2ecc9b2c02
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-16 08:19:39 +0530

    Time: 23 ms (10.72%), Space: 18.7 MB (5.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,29 +5,29 @@ public:
         stk.push(asteroids[0]);
         for(int i=1;i<asteroids.size();i++){
             if(asteroids[i]>=0){ // positive
-                if(!stk.empty() && stk.top()<0){ // negative
-                    // collision
-                    stk.push(asteroids[i]);
-                    while(stk.size()>=2){
-                        int tp1=stk.top();
-                        stk.pop();
-                        int tp2=stk.top();
-                        stk.pop();
-                        if((tp2<0 && tp1>0) || (tp1*tp2)>0){
-                            stk.push(tp2);
-                            stk.push(tp1);
-                            break;
-                        }else if(abs(tp1)>abs(tp2)){
-                            stk.push(tp1);
-                        }else if(abs(tp1)<abs(tp2)){
-                            stk.push(tp2);
-                        }
-                    }
-                }else{ // positive
-                    // push
-                    stk.push(asteroids[i]);
-                }
-                // stk.push(asteroids[i]);
+                // if(!stk.empty() && stk.top()<0){ // negative
+                //     // collision
+                //     stk.push(asteroids[i]);
+                //     while(stk.size()>=2){
+                //         int tp1=stk.top();
+                //         stk.pop();
+                //         int tp2=stk.top();
+                //         stk.pop();
+                //         if((tp2<0 && tp1>0) || (tp1*tp2)>0){
+                //             stk.push(tp2);
+                //             stk.push(tp1);
+                //             break;
+                //         }else if(abs(tp1)>abs(tp2)){
+                //             stk.push(tp1);
+                //         }else if(abs(tp1)<abs(tp2)){
+                //             stk.push(tp2);
+                //         }
+                //     }
+                // }else{ // positive
+                //     // push
+                //     stk.push(asteroids[i]);
+                // }
+                stk.push(asteroids[i]);
             }else{ // negative
                 if(!stk.empty() && stk.top()>=0){ // positive
                     // collision
