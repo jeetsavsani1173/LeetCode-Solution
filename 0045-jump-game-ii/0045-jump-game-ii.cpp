1c823de54040b9a9ea759edb02b485e2896d76a9
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-18 10:57:44 +0530

    Time: 601 ms (11.18%), Space: 17.8 MB (15.24%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int helper(int idx,vector<int> &nums,vector<int> &dp)
-    {
-        if(idx==(nums.size()-1)) return 0;
-
-        if(dp[idx]!=-1) return dp[idx];
+    int helper(int idx,vector<int> &nums,vector<int> &dp){
+        if(idx>=(nums.size()-1)) return 0;
+        
         int ans=INT_MAX-1;
-        for(int i=1;i<=nums[idx];i++)
-        {
-            if(idx+i<nums.size())
-            {
-                ans=min(ans,helper(idx+i,nums,dp));
-            }
+        if(dp[idx]!=-1) return dp[idx];
+        
+        for(int i=1;i<=nums[idx];i++){
+            ans=min(ans,1+helper(idx+i,nums,dp));
         }
-        return dp[idx]=1+ans;
+        
+        return dp[idx]=ans;
     }
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size()+1,-1);
         return helper(0,nums,dp);
     }
 };
