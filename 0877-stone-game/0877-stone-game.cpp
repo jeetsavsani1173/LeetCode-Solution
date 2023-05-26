682d046450240d7163820a27554a3c57b3b419da
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-05-26 07:18:52 +0530

    Time: 50 ms (10.24%), Space: 16.3 MB (31.94%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
+    int helper(int i,int j,vector<int> &p,vector<vector<int>> &dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=max(p[i]-helper(i+1,j,p,dp),p[j]-helper(i,j-1,p,dp));
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return helper(0,n-1,piles,dp)>0;
     }
 };
