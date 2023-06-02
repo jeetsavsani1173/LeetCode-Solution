2613d2e9579fa5c175aeb0f37bf0c1d0c000e9e0
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-06-02 10:03:06 +0530

    Time: 4 ms (73.33%), Space: 12 MB (37.96%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(int idx,int buy,vector<int> &prices,vector<vector<int>> &dp){
+        if(idx>=prices.size()) return 0;
+        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        int profit=0;
+        if(buy){
+            profit=max(helper(idx+1,0,prices,dp)-prices[idx],helper(idx+1,1,prices,dp));
+        }else{
+            profit=max(helper(idx+2,1,prices,dp)+prices[idx],helper(idx+1,0,prices,dp));
+        }
+        return dp[idx][buy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        return helper(0,1,prices,dp);
+    }
+};
