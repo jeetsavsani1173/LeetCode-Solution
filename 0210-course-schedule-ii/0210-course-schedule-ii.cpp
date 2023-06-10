cadaac896db386c88c419b00764565cbaa2e2b3a
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-06-10 08:01:05 +0530

    Time: 19 ms (90.59%), Space: 13.8 MB (55.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     vector<int> graph[2005];
-    vector<int> bfs(vector<vector<int>>& prerequisites,int n)
-    {
-        vector<int> indeg(n,0);
-        for(auto it:prerequisites)
-        {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indeg(numCourses,0);
+        for(auto it:prerequisites){
+            graph[it[1]].push_back(it[0]);
             indeg[it[0]]++;
         }
+        
         vector<int> ans;
+        
         queue<int> q;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<numCourses;i++){
             if(indeg[i]==0){
                 q.push(i);
                 ans.push_back(i);
             }
+        }
         
-        while(!q.empty())
-        {
-            int node=q.front();
+        while(!q.empty()){
+            int tp=q.front();
             q.pop();
             
-            for(auto it:graph[node])
-            {
-                indeg[it]--;
-                if(indeg[it]==0){
-                    q.push(it);
-                    ans.push_back(it);
+            for(auto child:graph[tp]){
+                indeg[child]--;
+                if(indeg[child]==0){
+                    q.push(child);
+                    ans.push_back(child);
                 }
             }
         }
         
-        if(ans.size()==n) return ans;
+        if(ans.size()==numCourses) return ans;
         return {};
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        for(auto it:prerequisites)
-            graph[it[1]].push_back(it[0]);
-        
-        // bfs
-        vector<int> ans= bfs(prerequisites,numCourses);
-        return ans;
     }
 };
