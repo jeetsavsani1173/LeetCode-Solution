c555096d3177fbabe422a2a2da7fc9e92bd61445
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-06-17 19:47:13 +0530

    Time: 121 ms (66.00%), Space: 67.7 MB (86.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -7,11 +7,8 @@ public:
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             maxSum=max(maxSum,sum);
-            
             if(sum<0){
                 sum=0;
-            }else{
-                maxSum=max(maxSum,sum);
             }
         }
         return maxSum;
