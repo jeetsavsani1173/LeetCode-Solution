e17dc6e5aa538cd1c9befd6d05690d624b563f02
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-06-30 09:48:11 +0530

    Time: 16 ms (71.41%), Space: 13.4 MB (14.28%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,8 +3,8 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
         
-        vector<int> row(n,0);
-        vector<int> col(m,0);
+        vector<bool> row(n,0);
+        vector<bool> col(m,0);
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
