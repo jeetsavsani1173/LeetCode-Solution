7095c234832c5bbcd15ed4a3bb00c0c4fad6c85b
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-06-30 10:49:26 +0530

    Time: 160 ms (20.47%), Space: 93.2 MB (74.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    // Make each day as potential selling-day or potential-buying day
     int maxProfit(vector<int>& prices) {
-        // here we are making each day as a selling-day..
-        int mn=prices[0];
-        int ans=0;
-        for(int i=1;i<prices.size();i++)
-        {
-            ans=max(ans,prices[i]-mn);
-            mn=min(mn,prices[i]);
+        int currMin=INT_MAX;
+        int currAns=0;
+        for(auto it:prices){
+            currMin=min(currMin,it);
+            currAns=max(currAns,it-currMin);
         }
-        return ans;
+        return currAns;
     }
 };
