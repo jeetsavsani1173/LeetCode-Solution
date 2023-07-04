d8eededa8d7b425baca80c3a176f2d1f45be016e
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-07-04 18:36:00 +0530

    Time: 10 ms (51.31%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -8,7 +8,6 @@ public:
         for(int i=1;i<n-1;i++){
             if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
         }
-        if(nums[n-2]!=nums[n-1]) return nums[n-1];
-        return 0;
+        return nums[n-1];
     }
 };
