80eb6d2eab7ec91db242fa26aaefe9ade029b3a5
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-08-27 09:22:01 +0530

    Time: 334 ms (22.74%), Space: 350.4 MB (9.59%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int helper(int idx,int next_move,vector<int> &stones,map<int,int> &mp,map<pair<int,int>,int> &dp){
+    int helper(int idx,int next_move,vector<int> &stones,map<int,int> &mp,vector<vector<int>> &dp){
         if(idx==stones.size()-1) return true;
-        if(dp.find({idx,next_move})!=dp.end()) return dp[{idx,next_move}];
+        if(dp[idx][next_move]!=-1) return dp[idx][next_move];
         bool flag1=false,flag2=false,flag3=false;
         // k-1 move
         if((next_move-1)>0 && mp.find(stones[idx]+(next_move-1))!=mp.end()){
@@ -19,7 +19,7 @@ public:
             flag3=helper(mp[stones[idx]+(next_move+1)],next_move+1,stones,mp,dp);
         }
         
-        return dp[{idx,next_move}]=flag1 || flag2 || flag3;
+        return dp[idx][next_move]=flag1 || flag2 || flag3;
     }
     bool canCross(vector<int>& stones) {
         map<int,int> mp;
@@ -27,7 +27,7 @@ public:
         for(int i=0;i<stones.size();i++){
             mp[stones[i]]=i;
         }
-        map<pair<int,int>,int> dp;
+        vector<vector<int>> dp(3000,vector<int>(3000,-1));
         return helper(0,0,stones,mp,dp);
     }
 };
