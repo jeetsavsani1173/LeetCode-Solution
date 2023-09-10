593e22a36019fe6eb72a298587995f0f55ed1107
Jeet Savsani <87079541+jeetsavsani1173@users.noreply.github.com>
2023-09-10 08:52:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (19.56%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        vector<long long> dp(n+1,0);
+        int M=pow(10,9)+7;
+        dp[0]=1;
+        for(long i=1;i<=n;i++)
+        {
+            dp[i]=dp[i-1]*i*(2*i-1);
+            dp[i]=dp[i]%M;
+        }
+        
+        return (dp[n]);
+    }
+};
